04d612526ae5b3e7bed2fbb16c5c40d49393f1ea
Josue García <josueemilio.gp24@gmail.com>
2026-01-16 02:58:37 -0600

    Acomodo de fotografia con respecto a los botones

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -57,19 +57,6 @@ export default function RecipePage() {
           <h1 className="mt-3 text-3xl font-bold">{recipe.title}</h1>
           <p className="mt-2 text-slate-100">{new Date(recipe.created_at).toLocaleString()}</p>
 
-          {recipe.photo_url && (
-            <div className="mt-6 overflow-hidden rounded-3xl border bg-white shadow-sm">
-              <Image
-                src={recipe.photo_url}
-                alt={`Foto de ${recipe.title}`}
-                width={1200}
-                height={800}
-                className="h-auto w-full object-cover"
-                priority
-              />
-            </div>
-          )}
-
           <button
             className="ml-4 rounded-xl border  text-slate-700 bg-white px-3 py-2 text-sm hover:bg-slate-50"
             onClick={() => router.push(`/recipe/${recipe.id}/edit`)}
@@ -83,6 +70,19 @@ export default function RecipePage() {
             >
             Seguir receta
           </button>
+
+          {recipe.photo_url && (
+            <div className="mt-6 overflow-hidden rounded-3xl border bg-white shadow-sm">
+              <Image
+                src={recipe.photo_url}
+                alt={`Foto de ${recipe.title}`}
+                width={1200}
+                height={800}
+                className="h-auto w-full object-cover"
+                priority
+              />
+            </div>
+          )}
 
           <div className="mt-8 grid gap-6">
             <section className="rounded-2xl border text-slate-700 bg-white p-6 shadow-sm">
